a796e2c79d6345572cfbd057eb15519f0b81e7f2
RiD_32 <rostokin.den@yandex.ru>
2018-01-31 23:15:22 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//components
+
+//styles
+import './less/App.less';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello, React</h1>
+    );
+  }
+}
+
+export default App;
